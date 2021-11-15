e71b0099e66ed8f23c54b43eab6da64ed759d900
Nikita <obodzinskiy.nikita@gmail.com>
2021-11-15 14:05:24 +0200

    3.3

---
+++ index.php
@@ -0,0 +1,133 @@
+<?php require_once 'header.php' ?>
+    <!-- first_section -->
+    <section class="first_section container">
+        <h1 class="headline">
+            Эффективные решения для вашего бизнеса
+        </h1>
+        <button type="button">
+            Заказать услугу
+        </button>
+    </section>
+    <!-- second section -->
+    <section class="container second_section">
+        <ul>
+        <!-- first adventage -->
+        <li>
+            <!-- headline adventage -->
+            <h3>
+                Внимание к деталям
+            </h3>     
+            <!-- body adventage -->
+            <p>
+            Идейные соображения, а также начало повседневной работы по формированию позиции.
+            </p>
+        </li>
+        <!-- second adventage -->
+        <li>
+            <!-- headline adventage -->
+            <h3>
+                Пунктуальность
+            </h3>              
+            <!-- body adventage -->
+            <p>
+                Задача организации, в особенности же рамки и место обучения кадров влечет за собой.
+            </p>            
+        </li>
+        <!-- third adventage -->
+        <li>
+            <!-- headline adventage -->
+            <h3>
+                Планирование
+            </h3>                
+            <!-- body adventage -->
+            <p>
+                Равным образом консультация с широким активом в значительной степени обуславливает.
+            </p>            
+        </li>
+        <!-- fourth adventage -->
+        <li>
+            <!-- headline adventage -->
+            <h3>
+                Современные технологии
+            </h3>
+            <!-- body adventage -->
+            <p>
+                Значимость этих проблем настолько очевидна, что реализация плановых заданий.
+            </p>                
+        </li>
+        </ul>
+    </section>
+    <!-- third section -->
+    <section class="container third_section">
+            <h2 class="headline">
+                Чем мы занимаемся
+            </h2>
+            <ul>
+                <li>
+                    <img src="img/home/third_section_first.jpg" width="370" height="300" alt="кодим"> <!-- такое описание пойдет? Думаю клиента бы улыбнуло такое описание:) --> 
+                </li>
+                <li>
+                    <img src="img/home/third_section_second.jpg" width="370" height="300" alt="дизайнерим">
+                </li>
+                <li>
+                    <img src="img/home/third_section_third.jpg" width="370" height="300" alt="играемся с цветами, вместо шрифтов">
+                </li>
+            </ul>
+    </section>
+    <!-- fourth section/our commande -->
+    <section class="container fourth_section">
+        <h2 class="headline">Наша команда</h2>    
+        <!-- card -->
+        <ul>
+            <li>
+                <!-- img_card -->
+                <img src="img/home/igor_demyanko.jpg" width="270" height="260" alt="Игорь Демьяненко">
+                <!-- headline_card -->
+                <h3>
+                    Игорь Демьяненко
+                </h3>    
+                <!-- body_card -->
+                <p>
+                    Product Designer    
+                </p>            
+            </li>
+            <li>
+                <!-- img_card -->
+                <img src="img/home/olga_repina.jpg" width="270" height="260" alt="Ольга Репина">        
+                <!-- headline_card -->
+                <h3>
+                    Ольга Репина
+                </h3>    
+                <!-- body_card -->
+                <p>
+                    Frontend Developer
+                </p>            
+            </li>
+            <li>
+                <!-- img_card -->
+                <img src="img/home/nikolai_tarasov.jpg" width="270" height="260" alt="Николай Тарасов">
+                <!-- headline_card -->
+                <h3>
+                    Николай Тарасов
+                </h3>         
+                <!-- body_card -->
+                <p>
+                    Marketing
+                </p>            
+            </li>
+            <li>
+                <!-- img_card -->
+                <img src="img/home/mihail_ermakov.jpg" width="270" height="260" alt="Михаил Ермаков">
+                <!-- headline_card -->
+                <h3>
+                    Михаил Ермаков
+                </h3>    
+                <!-- body_card -->
+                <p>
+                    UI Designer
+                </p>
+            </li>
+        </ul>
+    </section>
+    <!-- footer -->
+    <?php require_once 'footer.php' ?>
---
+++ portfolio.php
@@ -0,0 +1,88 @@
+    <?php require_once 'header.php' ?>
+    <!-- katalog -->
+    <section class="container katalog">
+        <div class="katalog_top">
+            <ul class="logic_button">
+                <li>
+                    <button type="logic_button" class="active_logic_button ">
+                        Все
+                    </button>
+                </li>
+                <li>
+                    <button type="logic_button" class="non_active_logic_button ">
+                        Веб-сайты
+                    </button>
+                </li>
+                <li>
+                    <button type="logic_button" class="non_active_logic_button ">
+                        Приложения
+                    </button>
+                </li>
+                <li>
+                    <button type="logic_button" class="non_active_logic_button ">
+                        Дизайн
+                    </button>
+                </li>
+                <li>
+                    <button type="logic_button" class="non_active_logic_button ">
+                        Маркетинг
+                    </button>
+                </li>
+            </ul>
+        </div>
+        <div class="katalog_price">
+            <ul>
+                <li>
+                    <img src="img/portfolio/tehnokryak.jpg" alt="card img">
+                    <h3>Технокряк</h3>
+                    <p>Веб-сайт</p>
+                </li>
+                <li>
+                    <img src="img/portfolio/design.jpg" alt="card img">
+                    <h3>Постер New Orlean vs Golden Star</h3>
+                    <p>Дизайн</p>
+                </li>
+                <li>
+                    <img src="img/portfolio/restaurant.jpg" alt="card img">
+                    <h3>Ресторан Seafood</h3>
+                    <p>Приложение</p>
+                </li>
+                <li>
+                    <img src="img/portfolio/project_prime.jpg" alt="card img">
+                    <div class="card_desc">
+                    <h3>Проект Prime</h3>
+                    <p>Маркетинг</p>
+                    </div>
+                </li>
+                <li>
+                    <img src="img/portfolio/project_boxes.jpg" alt="card img">
+                    <h3>Проект Boxes</h3>
+                    <p>Приложение</p>
+                </li>
+                <li>
+                    <img src="img/portfolio/inspiration_has_no_borders.jpg" alt="card img">
+                    <h3>Inspiration has no Borders</h3>
+                    <p>Веб-сайт</p>
+                </li>
+                <li>
+                    <img src="img/portfolio/limited_edition.jpg" alt="card img">
+                    <h3>Издание Limited Edition</h3>
+                    <p>Дизайн</p>
+                </li>
+                <li>
+                    <img src="img/portfolio/project_lab.jpg" alt="card img">
+                    <h3>Проект LAB</h3>
+                    <p>Маркетинг</p>
+                </li>
+                <li>
+                    <img src="img/portfolio/growing_business.jpg" alt="card img">
+                    <h3>Growing Business</h3>
+                    <p>Приложение</p>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <!-- footer -->
+
+
+    <?php require_once 'footer.php' ?>
